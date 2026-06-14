72562b533450237ad9794d89fbd1b75e60993282
hisaume <hisaume@proton.me>
2026-06-14 19:20:21 +0100

    Added frontend S3

---
+++ frontend_s3.tf
@@ -0,0 +1,33 @@
+resource "aws_s3_bucket" "frontend" {
+  bucket = var.frontend_bucket_name
+
+  tags = local.common_tags
+}
+
+// Private
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
