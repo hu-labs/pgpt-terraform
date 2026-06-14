1a2c6b3033a73a60b4e299d065e6d86bb5d15011
hisaume <hisaume@proton.me>
2026-06-14 13:21:33 +0100

    Edit by TF fmt

--- locals.tf
+++ locals.tf
@@ -3,12 +3,12 @@ data "aws_caller_identity" "current" {}
 locals {
   name_prefix = "${var.project}-${var.environment}"
 
-  lambda_name      = "${local.name_prefix}-lambda"
-  api_name         = "${local.name_prefix}-api"
-  usage_plan_name  = "${local.name_prefix}-usage-plan"
-  api_key_name     = "${local.name_prefix}-api-key"
-  frontend_origin  = "${local.name_prefix}-frontend-s3"
-  backend_origin   = "${local.name_prefix}-backend-api"
+  lambda_name     = "${local.name_prefix}-lambda"
+  api_name        = "${local.name_prefix}-api"
+  usage_plan_name = "${local.name_prefix}-usage-plan"
+  api_key_name    = "${local.name_prefix}-api-key"
+  frontend_origin = "${local.name_prefix}-frontend-s3"
+  backend_origin  = "${local.name_prefix}-backend-api"
 
   test_alias_name   = "Pgpt-lambda-test"
   stable_alias_name = "Pgpt-Lambda-stable"
