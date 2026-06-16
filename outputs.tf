3dec7c748b4fb5e4735c2cc06142ba49c56bcb1a
hisaume <hisaume@proton.me>
2026-06-16 19:47:46 +0100

    Expanded outputs

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "acm_validation_records" {
-  description = "Create these DNS records to validate the CloudFront certificate."
+  description = "Create these DNS records in Squarespace to validate the CloudFront certificate."
   value = {
     for dvo in aws_acm_certificate.preview.domain_validation_options : dvo.domain_name => {
       name  = dvo.resource_record_name
@@ -9,6 +9,63 @@ output "acm_validation_records" {
   }
 }
 
-output "preview_domain" {
-  value = var.preview_domain
+output "public_domain" {
+  value = var.public_domain
+}
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.preview.domain_name
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.preview.id
+}
+
+output "frontend_bucket_name" {
+  value = aws_s3_bucket.frontend.bucket
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.backend.function_name
+}
+
+output "lambda_test_alias" {
+  value = aws_lambda_alias.test.name
+}
+
+output "lambda_stable_alias" {
+  value = aws_lambda_alias.stable.name
+}
+
+output "api_gateway_id" {
+  value = aws_api_gateway_rest_api.api.id
+}
+
+output "api_gateway_test_url" {
+  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.aws_region}.amazonaws.com/test/chat"
+}
+
+output "api_gateway_prod_url" {
+  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.aws_region}.amazonaws.com/prod/chat"
+}
+
+output "public_api_url" {
+  value = "https://${var.public_domain}/api/v1/chat"
+}
+
+output "vite_api_url" {
+  value = "/api/v1/chat"
+}
+
+output "api_key_value" {
+  value     = random_password.api_gateway_key.result
+  sensitive = true
+}
+
+output "frontend_github_role_arn" {
+  value = aws_iam_role.frontend_deploy.arn
+}
+
+output "backend_github_role_arn" {
+  value = aws_iam_role.backend_deploy.arn
 }
